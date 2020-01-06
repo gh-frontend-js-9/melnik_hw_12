96555a72acffab1b01ae63479b603b1adcf43602
amarsik1 <artan9889@gmail.com>
2020-01-06 22:21:40 +0200

    added styles

--- components/messages/messages.js
+++ components/messages/messages.js
@@ -1,10 +1,10 @@
 class HTMLRender {
     static render(options) {
-        let {tags, className = [], text, title,value} = options;
+        let {tags, className = [], text, title, value} = options;
         let elem = document.createElement(tags);
         elem.classList.add(...className);
         elem.textContent = text;
-        elem.setAttribute(title,value);
+        elem.setAttribute(title, value);
         return elem;
     }
 }
